b6da0828705ff09d48e51b56989eceea24cf9f24
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-15 22:31:57 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,2 +1,21 @@
 class CouponsController < ApplicationController
+  def show
+  @coupon = Author.find(params[:id])
+end
+
+def new
+  @coupon = Author.new
+end
+
+def create
+     @coupon = Author.new(author_params)
+
+  if @coupon.valid?
+     @coupon.save
+
+     redirect_to author_path(@coupon)
+   else
+     render :new
+  end
+end
 end
